59c64552fdefd7464a1005d72d778d929465ce86
David <106503064+Dm0z@users.noreply.github.com>
2023-07-21 21:16:22 -0500

    Add files via upload

--- dots/bar/lemonbar_config.sh
+++ dots/bar/lemonbar_config.sh
@@ -1,48 +1,60 @@
 #!/bin/bash
 
-# Function to get the current audio level
-get_audio_level() {
-    amixer get Master | grep -oE '[0-9]+%' | head -1
+# Function to get the battery level using acpi
+get_battery_level() {
+    acpi | awk '{ print $4 }' | tr -d ','
 }
 
-# Function to check if audio is muted
+# Function to get the current time
+get_current_time() {
+    date '+%I:%M %p'
+}
+
+# Function to check if battery is charging
+is_charging() {
+    acpi | grep -qi "charging"
+}
+
+# Function to check if audio is muted using pacmd
 is_muted() {
-    amixer get Master | grep -q '\[off\]'
+    pactl list sinks | awk '/Mute:/{print $2}' | grep -q "yes"
 }
 
-# Function to toggle mute/unmute audio using pavucontrol
+# Function to toggle mute/unmute audio using pactl
 toggle_mute() {
-    pacmd set-sink-mute @DEFAULT_SINK@ toggle
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
 }
 
 # Function to adjust audio volume using pavucontrol
 adjust_volume() {
     if [ "$1" == "up" ]; then
-        pacmd set-sink-volume @DEFAULT_SINK@ +5%
+        pactl set-sink-volume @DEFAULT_SINK@ +5%
     elif [ "$1" == "down" ]; then
-        pacmd set-sink-volume @DEFAULT_SINK@ -5%
+        pactl set-sink-volume @DEFAULT_SINK@ -5%
     fi
-}
-
-# Function to get the battery level
-get_battery_level() {
-    acpi | awk '{ print $4 }' | tr -d ','
 }
 
 # Define font path
 font_path="/home/dmoz/.fonts/Iosevka Term Nerd Font Complete.ttf"
 
 while true; do
-    # Get the current time in the desired format
-    current_time=$(date '+%I:%M %p')
+    # Get the current time and battery information
+    current_time=$(get_current_time)
+    battery_level=$(get_battery_level)
 
-    # Construct the output string with date and time on the left side, battery level in the middle, audio level and mute indicator on the right side
-    output="%{l}Date: $(date '+%A %d %B') | Time: $current_time %{c}Battery: $(get_battery_level) %{r}"
+    # Construct the output string with date and time on the left side, battery level and charging status in the middle, audio level and mute indicator on the right side
+    output="%{l}Date: $(date '+%A %d %B') | Time: $current_time %{c}"
+
+    if is_charging; then
+        output+="Charging | "
+    fi
+
+    output+="Battery: $battery_level% %{r}"
 
     if is_muted; then
         output+="Muted"
     else
-        output+="Volume: $(get_audio_level)"
+        output+="Volume: $(pactl list sinks | awk '/Volume: front/{print $5}' | grep -oE '[0-9]+')%"
     fi
 
     echo "$output"
@@ -65,5 +77,6 @@ done | lemonbar -p -f "$font_path:size=10" -B "#00000000" -F "#FFFFFF" -g 1366x2
             ;;
     esac
 done
+
 
 
